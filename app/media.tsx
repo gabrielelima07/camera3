1ab8976f17fc882cef55073e2ac55a3deb5c5303
gabrielelima07 <gabriele.dibia07@gmail.com>
2025-08-27 19:41:44 -0300

    aula 27

--- app/media.tsx
+++ app/media.tsx
@@ -1,1 +1,12 @@
 import { Link, Stack, useLocalSearcParams, useRouter} from "expo-router";
+
+import {Alert, Image, Stylesheet} from "react-native";
+
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+import ObscuraButton from "@/components/ObscuraButton";
+
+import { saveToLibraryAsync } from "expo-media-library";
+
+
